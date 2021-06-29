4a4bbb9abbf5f36b03a15891ecab3498f0254ff6
luanateixeira148 <luanateixeira148@gmail.com>
2021-06-29 21:29:00 +0000

    implemented typewriter function

---
+++ typewriter.js
@@ -0,0 +1,23 @@
+const sentence = "hello there from lighthouse labs";
+
+const type = function(sentence) {
+  
+  let delay = 0;
+
+  for (let i = 0; i < sentence.length; i++) {
+  
+    setTimeout(() => {
+      process.stdout.write(sentence[i]);
+    }, delay);
+  
+    delay += 50;
+    
+  };
+  
+  setTimeout(() => {
+    process.stdout.write('\n');
+  }, delay)
+
+}
+
+type(sentence);
